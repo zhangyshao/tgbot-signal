5d00fb58b0e333bed2815185defb13d37ac750a3
zhangyshao <antonyxyz@163.com>
2026-02-04 17:28:30 +0800

    feat: add dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
+dotenv.config()
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.SERVER_PORT || 3000);
+
+  console.log("listening at: ", await app.getUrl())
 }
 bootstrap();
