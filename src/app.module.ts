1412e4ba6f03e1f505dd2575263ef1bad85e6f87
zhangyshao <antonyxyz@163.com>
2026-02-07 10:10:37 +0800

    feat: add global exception filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { modules } from './modules';
 import { GlobalModule } from './modules/global/module';
+import { APP_FILTER } from '@nestjs/core';
+import { GlobalExceptionFilter } from './common/filters/global';
 
 @Module({
   imports: [
@@ -8,6 +10,11 @@ import { GlobalModule } from './modules/global/module';
     ...modules
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter
+    },
+  ],
 })
 export class AppModule { }
